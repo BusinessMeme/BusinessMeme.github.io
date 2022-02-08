ad86fe878ce9c9596c72f1ce96e20e2272e5dd85
Vlad <druzhe.valdemar@gmail.com>
2022-02-08 11:37:22 +0200

    validation

--- src/js/script.js
+++ src/js/script.js
@@ -61,4 +61,35 @@ $(document).ready(function(){
       $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
     });
   });
+
+  function validateForms(form){
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите свое имя",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+              },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+              required: "Пожалуйста, введите свою почту",
+              email: "Неправильно введен адрес почты"
+            }
+        }
+    });
+};
+
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form');
 });
